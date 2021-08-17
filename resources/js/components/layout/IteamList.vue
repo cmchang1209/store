9f998e3f17205ab922e33d5bfddd86445ec424be
cmchang1209 <cmchang120956@gmail.com>
2021-08-17 07:12:46 +0000

    add home prod list

---
+++ resources/js/components/layout/IteamList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="ct-home-iteam-list">
+        <b-row align-v="baseline">
+            <b-col cols="8">
+                <h5>{{ data.title }}</h5>
+            </b-col>
+            <b-col cols="4" class="f-more-warp">
+                <router-link :to="data.link" class="f-more-link">
+                    查看更多 <b-icon-chevron-right></b-icon-chevron-right>
+                </router-link>
+            </b-col>
+        </b-row>
+        <div v-dragscroll.x="!isTouchDevice" class="no-scroll-bar table-responsive">
+            <table role="table" class="table b-table table-borderless">
+                <tbody>
+                    <tr>
+                        <td v-for="(iteam, index) in data.data" :key="index">
+                            <b-card title="" :img-src="iteam.imgSrc" img-alt="Image" img-top tag="article">
+                                <b-card-text>
+                                	<div class="text">{{ iteam.text }}</div>
+                                	<div class="price mt-3">{{ iteam.price }}</div>
+                                </b-card-text>
+                            </b-card>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+<script>
+import { dragscroll } from 'vue-dragscroll'
+import { mapGetters } from 'vuex'
+export default {
+    directives: {
+        dragscroll
+    },
+    components: {},
+    props: ['data'],
+    data() {
+        return {}
+    },
+    created() {},
+    computed: {
+        ...mapGetters(['isTouchDevice'])
+    },
+    methods: {}
+}
+
+</script>
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,16 +1,81 @@
 <template>
     <div class="ct-pc-main">
-    	<f-carousel />
+        <f-carousel />
+        <f-iteam-list v-for="(iteam, index) in list" :data="iteam" :key="index" />
     </div>
 </template>
 <script>
 import Carousel from '../components/layout/Carousel';
+import IteamList from '../components/layout/IteamList';
 export default {
     components: {
         'f-carousel': Carousel,
+        'f-iteam-list': IteamList,
     },
     data() {
-        return {}
+        return {
+            list: [{
+                    title: '主題',
+                    link: '',
+                    data: [{
+                            imgSrc: './images/prod1.png',
+                            text: '節日期間特別限定 - FIDODARTS 經 典主題系列 - 沈穩藍',
+                            price: 'NTD 30'
+                        },
+                        {
+                            imgSrc: './images/prod1.png',
+                            text: '節日期間特別限定 - FIDODARTS 經 典主題系列 - 活力橘',
+                            price: 'NTD 30'
+                        },
+                        {
+                            imgSrc: './images/prod1.png',
+                            text: '節日期間特別限定 - FIDODARTS 經 典主題系列 - 魅力紫',
+                            price: 'NTD 30'
+                        }
+                    ]
+                },
+                {
+                    title: '動畫',
+                    link: '',
+                    data: [{
+                            imgSrc: './images/prod2.png',
+                            text: 'FIDODARTS 獲獎動畫-四季季節',
+                            price: 'NTD 75'
+                        },
+                        {
+                            imgSrc: './images/prod2.png',
+                            text: '節日期間特別限定 - FIDODARTS 經 典主題系列 - 活力橘',
+                            price: 'NTD 30'
+                        },
+                        {
+                            imgSrc: './images/prod2.png',
+                            text: '節日期間特別限定 - FIDODARTS 經 典主題系列 - 活力橘',
+                            price: 'NTD 30'
+                        }
+                    ]
+                },
+                {
+                    title: '音效',
+                    link: '',
+                    data: [{
+                            imgSrc: './images/prod3.png',
+                            text: 'FIDODARTS 聖誕節日特別音效包',
+                            price: 'NTD 60'
+                        },
+                        {
+                            imgSrc: './images/prod3.png',
+                            text: '節日期間特別限定 - FIDODARTS 經 典主題系列 - 活力橘',
+                            price: 'NTD 30'
+                        },
+                        {
+                            imgSrc: './images/prod3.png',
+                            text: '節日期間特別限定 - FIDODARTS 經 典主題系列 - 活力橘',
+                            price: 'NTD 30'
+                        }
+                    ]
+                }
+            ]
+        }
     },
     created() {},
     methods: {}
